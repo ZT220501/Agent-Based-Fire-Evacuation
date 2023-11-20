d7d9a9e14c5c3f56c21d88492fa851795cd98b02
Yuxuan Liu <1145356576@qq.com>
2023-11-20 15:28:01 -0800

    Plot trajectories

--- main.m
+++ main.m
@@ -24,14 +24,30 @@ xlim([0 50])
 ylim([0 50])
 pause(0.2)
 
+all_X = zeros(iter_num, N, 2);
+
 for i=1:iter_num
     [X, V] = update(X, V, T/iter_num, @DOrsogna_Bertozzi_homo, @potential, e);
+    % [X, V] = update(X, V, T/iter_num, @Cucker_Smale_homo, @potential, e);
+
+    all_X(i,:,:) = X;
+
+
+    % scatter(X(:, 1), X(:, 2), 5, "blue", "filled")
     
+    for j = 1:N
+        plot(all_X(1:i,j,1), all_X(1:i,j,2), 'Color', [0.3010 0.7450 0.9330]);
+        if j == 1
+            hold on
+        end
+    end
+
+    scatter(e(:, 1), e(:, 2), 20, "red", "filled");
     
-    
-    scatter(X(:, 1), X(:, 2))
-    xlim([0 50])
-    ylim([0 50])
+    xlim([0 60])
+    ylim([0 60])
+    title(sprintf("iter num = %d", i))
+    hold off
     pause(0.2)
 end
 
